d588e0e6b232ac51ba74027c2d19a682be23e192
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2021-03-17 19:55:54 -0400

    add logger

--- lib/craft_bot.rb
+++ lib/craft_bot.rb
@@ -7,14 +7,16 @@ require "discordrb"
 module CraftBot
   class Error < StandardError; end
 
+  log = Logger.new
+
   # TODO: Set up propper logging
-  print "INFO - Initializing bot...\n"
+  log.info "Initializing bot..."
   bot = init_bot()
 
-  print "INFO - Initializing commands...\n"
+  log.info "Initializing commands..."
   init_commands(bot)
 
-  print "INFO - Starting CraftBot\n"
+  log.info "Starting CraftBot"
   bot.run
 
   def init_bot()
---
+++ lib/util/logger.rb
@@ -0,0 +1,14 @@
+class Logger
+  def info(log)
+    print "INFO -- #{log}\n"
+  end
+  def debug(log)
+    print "DEBUG -- #{log}\n"
+  end
+  def error(log)
+    print "ERROR -- #{log}\n"
+  end
+  def warn(log)
+    print "WARN -- #{log}\n"
+  end
+end
